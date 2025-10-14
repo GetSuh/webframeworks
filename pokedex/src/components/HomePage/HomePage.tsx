b9a8a14af8a3f4cb3ec0343032280e1ba22f3c24
Taguba Lance (s143847) <lance.taguba@student.ap.be>
2025-10-14 07:51:59 +0000

    Add HomePage and Root components with basic structure and styles
    - Created HomePage component with introductory text about Pokémon.
    - Created Root component with a simple navigation header.
    - Added corresponding CSS modules for HomePage and Root components.
    - Initialized global styles file.

---
+++ pokedex/src/components/HomePage/HomePage.tsx
@@ -0,0 +1,20 @@
+import styles from './HomePage.module.css';
+
+const HomePage = () => {
+  return (
+    <main>
+        <h1>Welcome to the world of Pokémon!</h1>
+        <p>
+            Hello there! I am Professor Oak.
+        </p>
+        <p>
+            This world is inhabited by creatures called Pokémon. For some people, Pokémon are pets. Others use them for battles.
+        </p>
+        <p>
+            Your journey to discover them all begins now!
+        </p>
+    </main>
+  );
+};
+
+export default HomePage;
---
+++ pokedex/src/components/Root/Root.tsx
@@ -0,0 +1,18 @@
+import styles from './Root.module.css'; 
+
+const Root = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Pokemon</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+    
+  );
+}
+export default Root;
